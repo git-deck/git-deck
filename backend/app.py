e4522efba739983e608e3d410a0815818f0fb9a8
Keigo Habara <k.habara.aa@gmail.com>
2021-06-28 00:05:56 +0900

    Use GraphQL

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 import requests
 from urllib.parse import parse_qs
 from flask import Flask, request, redirect, session, url_for, jsonify
-from github import Github
-from github.GithubObject import NotSet
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 from dotenv import dotenv_values
 import datetime
+import json
 
 
 # .env ファイルから環境変数を取得する
@@ -51,40 +52,66 @@ def callback():
     return redirect(url_for('user'))
 
 
+def github_client():
+    print("access_token:", session['access_token'])
+    return Client(
+        transport=RequestsHTTPTransport(
+            url = "https://api.github.com/graphql",
+            use_json = True,
+            headers = {
+                "Content-type": "application/json",
+                "Authorization": "Bearer {}".format(session['access_token'])
+            },
+            verify = False,
+            retries = 3,
+        ),
+        # これを使うとバグる. なんで?
+        #fetch_schema_from_transport=True,
+    )
+
+
 @app.route("/user")
 def user():
-    # sessionに保存してあるアクセストークンで PyGithubのGithubインスタンスを初期化する
-    g = Github(session['access_token'])
-    user = g.get_user()
-    return "Hello, {}".format(user.name)
+    resp = github_client().execute(
+        gql("""
+        query { 
+            viewer { 
+                login
+            }
+        }
+        """)
+    )
+    return "Hello, {}".format(resp["viewer"]["login"])
 
 
-# issueを表示
-@app.route("/repos/<repo>/issues")
-def issues(repo):
-    g = Github(session['access_token'])
-    user = g.get_user()
-    repo = g.get_repo("{}/{}".format(user.login, repo))
-    issues = repo.get_issues()
-    issues = list(map(lambda issue: {'title': issue.title, 'number': issue.number}, issues))
-    return jsonify(issues)
-
-
-# 指定されたissueのコメントを表示
-@app.route("/repos/<repo>/issues/<int:issue_number>/comments")
-def comments(repo, issue_number):
-    g = Github(session['access_token'])
-    user = g.get_user()
-    repo = g.get_repo("{}/{}".format(user.login, repo))
-    issue = repo.get_issue(issue_number)
-    comments = issue.get_comments()
-    comments = list(map(lambda comment: {'user': comment.user.login, 'body': comment.body}, comments))
-    return jsonify(comments) 
+# GraphQL に移行するため削除
+#
+# # issueを表示
+# @app.route("/repos/<repo>/issues")
+# def issues(repo):
+#     g = Github(session['access_token'])
+#     user = g.get_user()
+#     repo = g.get_repo("{}/{}".format(user.login, repo))
+#     issues = repo.get_issues()
+#     issues = list(map(lambda issue: {'title': issue.title, 'number': issue.number}, issues))
+#     return jsonify(issues)
+# 
+# 
+# # 指定されたissueのコメントを表示
+# @app.route("/repos/<repo>/issues/<int:issue_number>/comments")
+# def comments(repo, issue_number):
+#     g = Github(session['access_token'])
+#     user = g.get_user()
+#     repo = g.get_repo("{}/{}".format(user.login, repo))
+#     issue = repo.get_issue(issue_number)
+#     comments = issue.get_comments()
+#     comments = list(map(lambda comment: {'user': comment.user.login, 'body': comment.body}, comments))
+#     return jsonify(comments) 
 
 
 # 指定されたリポジトリのラベル一覧
 # e.g.
-# {
+# [
 #   {
 #     "color": "d73a4a", 
 #     "name": "bug"
@@ -94,75 +121,116 @@ def comments(repo, issue_number):
 #     "name": "documentation"
 #   }, 
 #   ...
-# }
-@app.route("/repos/<repo>/labels")
-def labels(repo):
-    g = Github(session['access_token'])
-    user = g.get_user()
-    repo = g.get_repo("{}/{}".format(user.login, repo))
-    labels = repo.get_labels()
-    labels = list(map(lambda label: {'name': label.name, 'color': label.color}, labels))
-    return jsonify(labels)
+# ]
+@app.route("/labels/<owner>/<repo>")
+def labels(owner, repo):
+    resp = github_client().execute(
+        gql("""
+        query($owner:String!, $repo:String!) {
+            repository(owner: $owner, name: $repo) {
+                labels(last:100) {
+                    edges {
+                        node {
+                            name
+                            color
+                        }
+                    }
+                }
+            }
+        }
+        """),
+        variable_values={
+            "owner": owner,
+            "repo": repo 
+        }
+    )
+    
+    ret = [edge["node"] for edge in resp["repository"]["labels"]["edges"]]
+    return jsonify(ret)
 
 
 # 指定したリポジトリのタイムライン取得
-# GET /repos/<repo>/timeline
-# GET /repos/<repo>/timeline?label=bug,documentation
-# GET /repos/<repo>/timeline?since=2018-06-29
-# label: labelでOR検索
-# since: updated_atが since以降のものだけとってくる
-# TODO: speed up
-@app.route("/repos/<repo>/timeline")
-def timeline(repo):
-    g = Github(session['access_token'])
-    user = g.get_user()
-    repo = g.get_repo("{}/{}".format(user.login, repo))
+# GET /timeline/<owner>/<repo>
+# x GET /timeline/<owner>/<repo>?label=bug,documentation
+# x GET /timeline/<owner>/<repo>?since=2018-06-29
+# x label: labelでOR検索
+# x since: updated_atが since以降のものだけとってくる
+@app.route("/timeline/<owner>/<repo>")
+def timeline(owner, repo):
+    #since = datetime.datetime.strptime(request.args['since'], '%Y-%m-%d') if 'since' in request.args else NotSet
+    #labels = request.args['label'].split(',') if 'label' in request.args else None
+    #issues(last:100, filterBy: {labels: ["bug","documentation"], states:OPEN, since: "2021-01-09T18:55:30.000Z"}) {
 
-    since = datetime.datetime.strptime(request.args['since'], '%Y-%m-%d') if 'since' in request.args else NotSet
-    labels = request.args['label'].split(',') if 'label' in request.args else None
-
-    issues = repo.get_issues(since=since)
+    resp = github_client().execute(
+        gql("""
+        query($owner:String!, $repo:String!) {
+            repository(owner: $owner, name: $repo) {
+                issues(last:100) {
+                    edges {
+                        node {
+                            number
+                            title
+                            url
+                            createdAt
+                            updatedAt
+                            body
+                            author {
+                                login
+                                url
+                                avatarUrl
+                            }
+                            assignees(last:100) {
+                                edges {
+                                    node {
+                                        login
+                                        url
+                                        avatarUrl
+                                    }
+                                }
+                            }
+                            labels(last:100) {
+                                edges {
+                                    node {
+                                        name
+                                        color
+                                    }
+                                }
+                            }
+                            comments(last:100) {
+                                edges {
+                                    node {
+                                        body
+                                        createdAt
+                                        updatedAt
+                                        url
+                                        author {
+                                            login
+                                            url
+                                            avatarUrl
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        """),
+        variable_values={
+            "owner": owner,
+            "repo": repo 
+        }
+    )
+    print("resp:", resp)
 
     timeline = []
+    for issue_node in resp["repository"]["issues"]["edges"]:
+        timeline.append(issue_node["node"])
 
-    for issue in issues:
-        # 指定されたlabelを一つも含まないissueとそのコメントはスキップ
-        if labels is not None and all([label not in map(lambda label: label.name, issue.labels) for label in labels]):
-            continue
-
-        timeline.append({
-            'assignees': [
-                {
-                    'avatar_url': assignee.avatar_url,
-                    'name': assignee.name,
-                    'url': assignee.url,
-                } for assignee in issue.assignees
-            ],
-            'body': issue.body,
-            'created_at': issue.created_at,
-            'updated_at': issue.updated_at,
-            'labels': [
-                {
-                    'name': label.name
-                } for label in issue.labels
-            ],
-            'number': issue.number,
-            'title': issue.title,
-            'url': issue.url,
-        })
-        for comment in issue.get_comments(since=since):
-            timeline.append({
-                'body': comment.body,
-                'created_at': comment.created_at,
-                'updated_at': issue.updated_at,
-                'url': comment.url,
-                'user': {
-                    'avatar_url': comment.user.avatar_url,
-                    'name': comment.user.name,
-                    'url': comment.user.url,
-                },
-            })
-
+        for comment_node in issue_node["node"]["comments"]["edges"]:
+            timeline.append(comment_node["node"])
+        
     # updated_at が新しい順に並べる
-    timeline.sort(key=lambda tweet: tweet['updated_at']).reverse()
-    return jsonify(timeline)
+    print("len:", len(timeline))
+    return jsonify(list(reversed(sorted(timeline, key=lambda tweet: tweet['updatedAt']))))
