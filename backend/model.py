8d4543b65551ffd27c1050ef1e375ee09d60375c
Keigo Habara <k.habara.aa@gmail.com>
2021-06-28 06:51:42 +0900

    refactor model.py

--- backend/model.py
+++ backend/model.py
@@ -1,12 +1,6 @@
 from datetime import datetime
 from database import db
 
-#class User(db.Model):
-#    __tablename__ = 'users'
-#    id = db.Column(db.Integer, primary_key=True)
-#    name = db.Column(db.String(255), nullable=False)
-#    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
-#    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
 class Idea(db.Model):
     __tablename__ = 'ideas'
