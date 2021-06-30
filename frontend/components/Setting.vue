ed86133e09bbe98aa84fab68f96e21e533d636f7
zwwaa-ku <n.miyazawa2412@gmail.com>
2021-07-01 00:12:55 +0900

    label implement

--- frontend/components/Setting.vue
+++ frontend/components/Setting.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>ヤッホー</div>
+  <div class="label_column">
+    <header>
+      <button class="filter_list" @click="clickSettings">
+        <span class="material-icons"> filter_list </span>
+      </button>
+      <span class="m">ラベルで絞り込む</span>
+
+      <div class="labels">
+        <Label :color="color" :message="message"></Label>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
-export default Vue.extend({})
+import Label from '@/components/Lavel.vue'
+type DataType = {
+  style: string
+  message: string
+}
+export default Vue.extend({
+  components: {
+    Label,
+  },
+  data() {
+    return {
+      color: '#ff0f00',
+      message: 'ex_label',
+    }
+  },
+})
 </script>
