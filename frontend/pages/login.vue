40340f347456bb0587c0933133ffb579cc8e7218
knknk98 <kayooshino@gmail.com>
2021-07-02 02:13:08 +0900

    ログインページの大枠

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,44 +1,19 @@
 <template>
-  <div class="container">ログイン画面</div>
+  <div class="container">
+    <div class="login-page">
+      <div class="caption">
+        <div class="title">GitDeck</div>
+        <div class="subtext">
+          いい感じの説明が入リますいい感じの説明が入リますいい感じの説明が入リますいい感じの説明が入リますいい感じの説明が入リますいい感じの説明が入リます
+        </div>
+      </div>
+      <button>GitHubでログイン</button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import VModal from 'vue-js-modal'
 
-Vue.use(VModal)
-
-type DataType = {
-  timeline: Object[]
-}
-export default Vue.extend({
-  data(): DataType {
-    return {
-      timeline: [
-        {
-          owner: 'knknk98',
-          repo: 'issue-twitter',
-          ownerUrl: 'https://github.com/knknk98',
-          repoUrl: 'https://github.com/knknk98/issue-twitter',
-        },
-        {
-          owner: 'knknk98',
-          repo: 'issue-twitter',
-          ownerUrl: 'https://github.com/knknk98',
-          repoUrl: 'https://github.com/knknk98/issue-twitter',
-        },
-      ],
-    }
-  },
-  methods: {
-    append(owner: string, repo: string) {
-      this.timeline.push({
-        owner,
-        repo,
-        ownerUrl: 'https://github.com/' + owner,
-        repoUrl: 'https://github.com/' + owner + '/' + repo,
-      })
-    },
-  },
-})
+export default Vue.extend({})
 </script>
