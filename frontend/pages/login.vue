d30e7a3e4921de713bbe1ee782de2ac88361e993
knknk98 <kayooshino@gmail.com>
2021-07-02 03:01:23 +0900

    ログインページUIを修正

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -7,13 +7,32 @@
           いい感じの説明が入リますいい感じの説明が入リますいい感じの説明が入リますいい感じの説明が入リますいい感じの説明が入リますいい感じの説明が入リます
         </div>
       </div>
-      <button>GitHubでログイン</button>
+      <div class="login-button">
+        <div class="caption">login with GitHub account</div>
+        <button>
+          <Octicon :icon="Octicons.markGithub" class-name="github-mark" />Log in
+        </button>
+        <div class="link">
+          New to GitHub?
+          <a href="https://github.com/signup" target="_blank">Sign up now »</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
-export default Vue.extend({})
+const { Octicon, Octicons } = require('octicons-vue')
+type DataType = {
+  Octicons: any
+}
+export default Vue.extend({
+  components: { Octicon },
+  data(): DataType {
+    return {
+      Octicons,
+    }
+  },
+})
 </script>
