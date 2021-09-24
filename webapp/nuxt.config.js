42bae307ba3c25f5010bb77bb5eee7c463609d07
keigo habara <34413567+habara-k@users.noreply.github.com>
2021-09-24 23:58:16 +0900

    Fix: SSRに戻した (#202)

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  // ssr: false,
-
-  target: "static",
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
