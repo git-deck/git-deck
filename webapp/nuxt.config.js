eca0c83759018f0e1e7fcfbfbfe0346bc629071d
Keigo Habara <k.habara.aa@gmail.com>
2021-09-30 14:28:11 +0900

    enable SSR

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  // ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
