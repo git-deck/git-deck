eed21cd49762edcbbf224f15e4ec393ed5bbfa3c
keigo habara <34413567+habara-k@users.noreply.github.com>
2021-09-30 17:25:47 +0900

    enable SSR (#227)
    * enable SSR
    
    * Remove deploy on merging to main
    
    * Add target: 'static'
    
    * Fix env
    
    * Remove deploy on PR

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  // ssr: false,
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
