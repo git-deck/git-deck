732ef19b3910527232c53f4ebe4f5e1a995e9720
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-10-15 22:36:24 +0900

    ログイン失敗時のエラー表示を追加

--- webapp/plugins/getAccessToken.ts
+++ webapp/plugins/getAccessToken.ts
@@ -7,12 +7,22 @@ const getAccessTokenPlugin: Plugin = async ({ $accessor }) => {
   if (accessToken != null) {
     $accessor.auth.setGithubAccessToken(getAccessTokenFromLocalStorage())
   } else {
-    const result = await firebase.auth().getRedirectResult()
-    // @ts-ignore
-    const accessToken =
-      (result.credential as firebase.auth.OAuthCredential | null)
-        ?.accessToken ?? null
-    $accessor.auth.setGithubAccessToken(accessToken as string | null)
+    try {
+      const result = await firebase.auth().getRedirectResult()
+      // @ts-ignore
+      const accessToken =
+        (result.credential as firebase.auth.OAuthCredential | null)
+          ?.accessToken ?? null
+      $accessor.auth.setGithubAccessToken(accessToken as string | null)
+    } catch (e: any) {
+      if (e.code === 'auth/web-storage-unsupported') {
+        window.alert(
+          'このブラウザはサポートされていないか、サードパーティのクッキーやデータが無効になっている可能性があります。'
+        )
+      } else {
+        throw e
+      }
+    }
   }
 }
 
